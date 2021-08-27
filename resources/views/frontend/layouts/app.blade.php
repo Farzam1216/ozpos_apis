11fd6661df27303b260553ca0cc6060c8966f05f
Abdullah Khan <abdullahkhanwork@gmail.com>
2021-08-27 17:04:33 +0500

    smooth scroll for cuisine click fix

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -165,7 +165,7 @@
                     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
                        if (target.length) {
                          $('html,body').animate({
-                             scrollTop: target.offset().top -75
+                             scrollTop: target.offset().top - 60
                         }, 800);
                         return false;
                     }
